544142753b3867b1ad09cbdd87361bbd0f4c65bb
Daniele Ciminieri <stonecoldcrazy@gmail.com>
2015-10-20 17:58:28 +0200

    dded play state

--- www/js/controllers/play.js
+++ www/js/controllers/play.js
@@ -4,11 +4,37 @@ app.controller('PlayCtrl', function($scope,$state,socket, playlistService) {
     $scope.chunks = playlistService.getPlaylist();
     $scope.currchunk = $scope.chunks[0];
     $scope.selectedChunk = $scope.currchunk.id;
+    $scope.play = true;
+
+  $scope.selectVideo = function(vid) {
+    $scope.selectedChunk = vid.id;
+    $scope.currchunk = vid;
+
+  };
 
 
+  $scope.playOrPause = function() {
+    $scope.play = !$scope.play;
 
+    if($scope.play){
+      socket.emit('playStatus',"play");
+    }
+    else {
+      socket.emit('playStatus',"pause");
+    }
+
+  }
 
     $scope.go = function(where) {
         $state.go(where);
     }
+
+  $scope.$watch('selectedChunk',function(newValue,oldValue){
+    console.log(newValue,oldValue)
+    socket.emit('playChunk',newValue);
+
+  })
+
 })
+
+
--- www/js/directives/timeline.js
+++ www/js/directives/timeline.js
@@ -4,10 +4,16 @@ app.directive('timeline', function() {
         templateUrl: 'views/timeline.html',
         link: function(scope, iElement, iAttrs) {
 
+       //   scope.totalTime = _.sum(scope.chunks,'duration');
+
             scope.$watchCollection('chunks',function(oldValue,newValue){
                scope.totalTime = _.sum(oldValue,'duration');
 
             })
+
+          scope.computeWidth = function(duration){
+            return Math.floor((duration/scope.totalTime)*100)+'%';
+          }
 
         }
     }
